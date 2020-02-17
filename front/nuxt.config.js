9b16b824511f55cb38a8be00f1c809c5d98a246a
Mason Xiong <mason.xiong@yahoo.com>
2020-02-17 14:55:08 +1100

    remove eslint

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -35,8 +35,8 @@ module.exports = {
    ** Nuxt.js dev-modules
    */
   buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
+    // // Doc: https://github.com/nuxt-community/eslint-module
+    // '@nuxtjs/eslint-module',
     '@nuxtjs/vuetify'
   ],
   /*
