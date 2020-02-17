a3bfa00a9004c8745aadc86a341c5b1597da0a10
Shaun Voigt <shaunv.1000@gmail.com>
2020-02-17 15:52:41 +1100

    Fix json output

--- atlas/atlas_proxy.py
+++ atlas/atlas_proxy.py
@@ -10,12 +10,16 @@ from urllib.parse import urlparse, parse_qs
 
 FILE = 'proxy_test.html'
 PORT = 8080
+ATLAS_API_KEY = "ba0ea367-df59-4cfb-8e50-3fc8a2fd9243"
 
 API='/api/v1'
+MY_RESULTS_REQ=API+'/mine'
 PING_START_REQ=API+'/ping'
 TRACEROUTE_START_REQ=API+'/traceroute'
 PING_RESULT_REQ=API+'/pingresult'
 TRACEROUTE_RESULT_REQ=API+'/tracerouteresult'
+
+# /api/v2/measurements/my/?key=ba0ea367-df59-4cfb-8e50-3fc8a2fd9243
 
 class TestHandler(http.server.SimpleHTTPRequestHandler):
     """The test example handler."""
@@ -40,6 +44,8 @@ class TestHandler(http.server.SimpleHTTPRequestHandler):
             self.getPingResults(query_params)
         elif path == TRACEROUTE_RESULT_REQ:
              self.getTracerouteResults(query_params)
+        elif path == MY_RESULTS_REQ:
+            self.getMyResults(query_params)
         else:
             print("Invalid Query:")
             print(path)
@@ -48,12 +54,17 @@ class TestHandler(http.server.SimpleHTTPRequestHandler):
     def getPingResults(self, query_params):
         source = "https://atlas.ripe.net/api/v2/measurements/23976423/results/"
         responses = requests.get(source).json()
-        self.wfile.write(str(responses).encode())
+        self.wfile.write(json.dumps(responses, ensure_ascii=False).encode())
 
     def getTracerouteResults(self, query_params):
         source = "https://atlas.ripe.net/api/v2/measurements/23976424/results/"
         responses = requests.get(source).json()
-        self.wfile.write(str(responses).encode())    
+        self.wfile.write(json.dumps(responses, ensure_ascii=False).encode())  
+
+    def getMyResults(self, query_params):
+        source = "https://atlas.ripe.net/api/v2/measurements/my/?key=%s"%ATLAS_API_KEY
+        responses = requests.get(source).json()
+        self.wfile.write(json.dumps(responses, ensure_ascii=False).encode())
 
 def start_server():
     """Start the server."""
